93446e12233c44ac02708e4c230e2eb9dfc0a395
programmermosharof <mosharof.dev@gmail.com>
2026-01-28 11:07:31 +0600

    Fundamentals of String & Objects

---
+++ index1.js
@@ -0,0 +1,19 @@
+const country = 'Bangladesh';
+
+const divtion = 'Dhaka';
+
+const disstict = 'Kishorgonj';
+// const thana = new String('Dhanmondi') ;
+
+// const number = [12, 32, 45, 56, 67, ] ;
+
+// console.log(number.length);
+
+const captal = 'Dhaka';
+
+console.log(captal.length);
+console.log(captal[3]);
+
+
+
+
