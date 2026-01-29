b7086994cd0e450045605a5c1790cbf0b00e2e27
programmermosharof <mosharof.dev@gmail.com>
2026-01-29 09:03:26 +0600

    All  error fix

---
+++ task7.js
@@ -0,0 +1,46 @@
+// // let arr = [1, 2, 3, 4];
+// // arr.reverse();
+// // console.log(arr);
+
+
+// const friend = [ "mahin", "fahim", "mosharof", "Munsef"];
+
+// for(let i = 0; i < friend.length; i++){
+//     console.log(i);
+//     console.log(friend[i]);
+// }
+
+
+
+// // Reversing an array with/without reverse method
+
+
+// // let arr = [10, 20, 30, 40];
+// // let reversed = [];
+
+// // for (let i = arr.length - 1; i >= 0; i--) {
+// //   reversed.unshift(arr[i]);
+// // }
+
+// // console.log(reversed);
+
+// // let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// // for (let i = num.length - 1; i >= 0; i--) {
+// //   const number = num[i];
+// //   console.log(number);
+// // }
+
+
+// let num = [1,2,3,4,5,6,7,8,9];
+// let reversed = [];
+
+// for (let i = num.length - 1; i >= 0; i--) {
+//   reversed.push(num[i]);
+// }
+
+// console.log(reversed);
+
+let nums = [2, 1, 7, 9,  5];
+nums.sort();
+console.log(nums);
